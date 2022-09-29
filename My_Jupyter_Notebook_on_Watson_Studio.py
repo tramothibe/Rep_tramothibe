58649da0785415660dfcd839f0ec9d1dbc57cd3c
tramothibe <114567516+tramothibe@users.noreply.github.com>
2022-09-29 19:59:56 +0200

    Add files via upload
    Markdown

---
+++ My_Jupyter_Notebook_on_Watson_Studio.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Teboho Ramothibe** <br> Systems Engineer
+
+# *I am interested in data science because I don't think there's a better way to solve unknown unknowns*
+
+# ### The following code adds two random integers
+
+# In[5]:
+
+
+1+1
+ 
+
+
+# #### Strikethrough
+# ~~Can we finish this cause already?~~. No, there's still lots of fun ahead. <br>
+# 
+# #### Bullet list
+# Please list baking ingredients for a cake:
+# * Flower
+# * Milk
+# * Eggs
+# * Cheese
+# * Sugar
+# 
+# #### Table
+# 
+# |Markdown||is||Awesome|
+# |--------||--------||--------|
+# |Make||Notes||Today!|
+# |1||2||GO!|
+
+# In[ ]:
+
+
+
+
